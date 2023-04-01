19b9ab2f19ddf2ae0bfb305c4cef2cc5d47af302
Yash Jadwani <yash.jadwani1998@gmail.com>
2023-04-02 00:31:05 +0100

    Side by side image

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ from tensorflow import keras
 from tensorflow.keras.models import load_model
 from tensorflow.keras import preprocessing
 import time
+import warnings
 
-fig = plt.figure(figsize=(5,5))
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+fig = plt.figure(figsize=(3,3))
+
+st.set_page_config(layout="wide")
 
 with open("custom.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -21,23 +26,37 @@ st.markdown("Upload a grayscale image of a handwritten digit (28x28 pixels)")
 
 def main():
     file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
-    class_btn = st.button("Classify")
     if file_uploaded is not None:    
         image = tf.keras.preprocessing.image.load_img(file_uploaded, target_size=(28, 28), color_mode='grayscale')
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+    class_btn = st.button("Classify")
         
+    predictions = None
+
     if class_btn:
         if file_uploaded is None:
             st.write("Invalid command, please upload an image")
         else:
             with st.spinner('Model working....'):
-                plt.imshow(image)
-                plt.axis("off")
+            
                 predictions = predict(image)
                 time.sleep(1)
-                st.success('Classified')
-                st.write(predictions)
-                st.pyplot(fig)
+
+                st.success(predictions)
+
+    
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.markdown('<h3 style="text-align:centre;" >Uploaded Image</h3>',unsafe_allow_html=True)
+                    st.image(file_uploaded, use_column_width= True)
+                with col2:
+                    if predictions is not None:
+                        st.markdown('<h3 style="text-align:centre;" >HeatMap of the uploaded Image</h3>',unsafe_allow_html=True)
+                        plt.imshow(image)
+                        plt.axis("off")
+                        st.pyplot(fig)
+                        
+
+                
 
 
 def predict(image):
